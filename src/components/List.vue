2b9b73c9754e4032f8d20a29f03ceec4be3d17b6
lucy <lovinglife.lm@gmail.com>
2022-02-10 20:18:40 -0600

    Refactor done

---
+++ src/components/List.vue
@@ -0,0 +1,37 @@
+<script setup>
+import useTodos from "../composables/useTodos";
+
+const { pending, completed, changeStatus } = useTodos();
+
+defineProps({
+isCompleted: {
+default: false,
+type: Boolean,
+},
+});
+
+</script>
+
+<template>
+    
+    
+<div class="w-1/3">
+<h3 class="text-4xl font-bold text-center"  :class="isCompleted ?  'text-yellow-600' : 'text-purple-300' ">{{ isCompleted ? 'Completed' : 'Pending' }}</h3>
+<ul class="pt-8 space-y-4">
+  <li 
+  v-for="todo in isCompleted ? completed : pending "
+  :key="todo.id"
+  @click="changeStatus(todo.id)"
+  :class="
+  isCompleted 
+  ? 'text-yellow-600  hover:bg-purple-300'
+   :'text-purple-300  hover:bg-blue-400'"
+  class= "w-full px-4 py-2 font-bold text-center bg-slate-400 bg duration-500 rounded-lg hover:cursor-pointer  hover:text-gray-200">
+
+ {{ todo.content }}
+  </li>
+   
+</ul>
+</div>
+
+</template>
---
+++ src/components/Title.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import useTodos from "../composables/useTodos";
+const { addTodo } = useTodos();
+const newTodo = ref("");
+const add = () => {
+  if (newTodo.value) {
+  addTodo(newTodo.value);
+  newTodo.value = "";
+  }
+};
+</script>
+
+<template>
+
+  <h1 class="pb-4 text-6xl font-thin tracking-tight text-center text-orange-300 "> 
+  🎶🤨My Todo App</h1>
+<input 
+  @change="add"
+  v-model="newTodo"
+  type="text" 
+  class="px-4 py-2 text-xl text-center rounded-lg" placeholder="New Todo" />
+
+    
+</template>
